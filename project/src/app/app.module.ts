1831568680be3e9c836f20e63cdaf38c9bf5ad42
Denislav <denis1005@users.noreply.github.com>
2022-12-08 23:18:27 +0200

    Creating header component

---
+++ project/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from "./core/core.module";
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CommonModule,
+        CoreModule
+    ]
+})
+export class AppModule { }
