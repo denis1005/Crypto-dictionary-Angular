dd6bb4a134766d45c1db1286f277e68fc42be09a
Denislav <denis1005@users.noreply.github.com>
2022-12-09 00:02:09 +0200

    Implementing routing

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main/main.component';
 
 const routes: Routes = [
   {
     path:'',
-    
+    pathMatch:'full',
+    component:MainComponent
   }
 ];
 
