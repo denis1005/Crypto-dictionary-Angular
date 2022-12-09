e09b1d7937aedee5a02e153899477a3cb6349e24
Denislav <denis1005@users.noreply.github.com>
2022-12-09 13:38:08 +0200

    Creating crypto-tracer component

---
+++ project/src/app/main/cryptotraker/cryptotraker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cryptotraker',
+  templateUrl: './cryptotraker.component.html',
+  styleUrls: ['./cryptotraker.component.css']
+})
+export class CryptotrakerComponent {
+
+}
--- project/src/app/main/main.module.ts
+++ project/src/app/main/main.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { CryptocardsComponent } from './cryptocards/cryptocards.component';
+import { CryptotrakerComponent } from './cryptotraker/cryptotraker.component';
 
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    CryptocardsComponent
+    CryptocardsComponent,
+    CryptotrakerComponent
   ],
   imports: [
     CommonModule
